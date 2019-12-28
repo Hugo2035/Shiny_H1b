0cdfe41f53c5f02483501a34441a493bf66dd91e
Suraj Malpani <31307028+SurajMalpani@users.noreply.github.com>
2019-12-27 17:38:38 -0800

    Update global.R

--- global.R
+++ global.R
@@ -1,6 +1,6 @@
 
 #Loading dependent packages  ----
-rqrd_Pkg = c('shiny','data.table','plotly','plyr','tidyverse','wordcloud2','ggmap')
+rqrd_Pkg = c('shiny','plotly','plyr','tidyverse','ggmap')
 for(p in rqrd_Pkg){
   if(!require(p,character.only = TRUE)) 
     install.packages(p);
